6af3aeea7133a1eb7f06a048ff23c9df543c0a2d
DiegoMicali <diego.micali13@gmail.com>
2026-03-26 14:21:04 -0300

    feat: create controller, entity and  service classes, repository and migrations tables for movies

---
+++ src/main/java/com/movieflix/entity/Movie.java
@@ -0,0 +1,54 @@
+package com.movieflix.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(name = "realease_date")
+    private LocalDate releaseDate;
+
+    private double rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "update_at")
+    private LocalDate updatedAt;
+
+    @ManyToMany
+    @JoinTable(name = "movie_category",
+    joinColumns = @JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private List<Category> categories;
+
+    @ManyToMany
+    @JoinTable(name = "movie_streaming",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "streaming_id"))
+    private List<Streaming> streamings;
+}
