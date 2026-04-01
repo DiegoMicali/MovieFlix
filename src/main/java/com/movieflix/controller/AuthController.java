767cbec525d33a7bc91efc05209767c4c6b22cf0
DiegoMicali <diego.micali13@gmail.com>
2026-04-01 14:48:01 -0300

    feat: implement Username or Password not found error exception handler

--- src/main/java/com/movieflix/controller/AuthController.java
+++ src/main/java/com/movieflix/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.movieflix.controller;
 
 import com.movieflix.config.TokenService;
 import com.movieflix.entity.User;
+import com.movieflix.exception.UsernameOrPasswordInvalidException;
 import com.movieflix.mapper.UserMapper;
 import com.movieflix.request.LoginRequest;
 import com.movieflix.request.UserRequest;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,6 +39,7 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        try {
         UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password());
         Authentication authentication = authenticationManager.authenticate(userAndPass);
 
@@ -45,6 +48,10 @@ public class AuthController {
         String token = tokenService.geneteToken(user);
 
         return ResponseEntity.ok(new LoginResponse(token));
+
+        } catch(BadCredentialsException e) {
+            throw new  UsernameOrPasswordInvalidException("Usuário ou senha inválido.");
+        }
     }
 
 }
